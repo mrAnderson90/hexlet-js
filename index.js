603a31f8de3436e0d43056ba48297dea20d26d7a
mrAnderson90 <tyler.andreev@mail.ru>
2022-11-29 18:44:40 +0300

    Fix index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import _ from "lodash";
+import _ from 'lodash';
 
-console.log("Hello, Hexlet!");
+console.log('Hello, Hexlet!');
 
-console.log(_.last(["one", "two"]));
+console.log(_.last(['one', 'two']));
 
 const fn = (a, b) => {
   const c = a + b;
